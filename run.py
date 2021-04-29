0cda1aaf8f537eb3e8425609844267509a9bcdaf
131sss <131sss@users.noreply.github.com>
2021-04-29 23:15:07 +0200

    v0.0.1

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
+@app.route('/gallery/')
+def gallery():
+    return render_template("gallery.html")
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
 
 
 
