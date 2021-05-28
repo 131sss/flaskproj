1229a8bb60e9ec17a1c07b2fadecdd019a0d7746
131sss <131sss@users.noreply.github.com>
2021-05-29 01:54:31 +0200

    v1.0 (beta)

---
+++ azurecred.py
@@ -0,0 +1,5 @@
+AZDBSERVER = 'wsbsqldb.database.windows.net' 
+AZDBNAME = 'WSBAzureDB' 
+AZDBUSER = 'Oleksandr' 
+AZDBPW = '1QW3e4r5' 
+AZDBDRIVER = '{ODBC Driver 17 for SQL Server}'
--- run.py
+++ run.py
@@ -1,25 +1,76 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-from datetime import datetime
-from flask import abort
+from flask import abort, redirect, url_for,flash
+from AzureDB import AzureDB
+from flask_dance.contrib.github import make_github_blueprint, github
+import secrets
+import os
+
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+github_blueprint = make_github_blueprint(
+    client_id="fdbd95e61ea776e227a5",
+    client_secret="72a05833b2ac5c88dc6a63e3d63b6de29096bf5d"
+
+)
+app.register_blueprint(github_blueprint, url_prefix='/login')
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def github_login():
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    else:
+        account_info = github.get('/user')
+        if account_info.ok:
+            return render_template('index.html')
+    return '<h1>Request failed!</h1>'   
+
 @app.route('/gallery/')
 def gallery():
     return render_template("gallery.html")
 @app.route('/about/')
 def about():
     return render_template("about.html")
-@app.route('/contact/')
+
+@app.route('/contact', methods=['POST','GET'])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        if len(request.form['name']) > 2 and len(request.form['mail']) > 3 and len(request.form['text']) > 5:
+            res = AzureDB().azureAddData(request.form['name'],request.form['mail'],request.form['text'])
+            flash('Comentarz został wysłąny', category='success')
+        
+        else:
+            flash('Niepoprawna forma komentarza, sprawdz', category='error')
+    
+    return render_template('contact.html')
 
+@app.route('/comments')
+def comments():
+    data = AzureDB().azureGetData()
+    return render_template("comments.html", data = data)
 
+@app.route('/comments/<int:id>/delete')
+def delete_comm(id):
+    AzureDB().azureDeleteData(id)
+    return redirect('/comments')
+
+@app.route("/comments/<int:id>/edit", methods=['POST', 'GET'])
+def edit_comm(id):
+    comm = AzureDB().azureGetDataid(id)
+    if request.method == "POST":
+        if len(request.form['text']) > 3:
+            res = AzureDB().azureEditData(request.form['text'], id)
+            return redirect('/comments')
+        
+        else:
+            return 'Nie możesz zostawić pusty komentarz!'
+    else:
+        
+        return render_template('edit.html', comm = comm)
 
 @app.route('/error_denied')
 def error_denied():
